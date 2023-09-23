cd98e1f7f380733a184b44081841cd910c81fc1a
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-09-23 09:38:08 -0700

    Add files via upload

---
+++ program 48.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num =1;
+	while(num <= 5)
+	{
+	 
+	cout << "Number=" << num << "Square=" << num*num << endl;
+	cout << "Number=" << num << " Cube=" << num*num*num << endl;
+	num++;
+   }
+	return 0;
+}
